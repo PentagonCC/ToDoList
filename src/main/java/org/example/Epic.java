77c363b4e98698ca8fa2ac78ffba8106e11d336b
Nikita <iv-nikita99@mail.ru>
2025-02-02 18:15:01 +0300

    template application

--- src/main/java/org/example/Epic.java
+++ src/main/java/org/example/Epic.java
@@ -5,56 +5,16 @@ import java.util.HashMap;
 public class Epic extends Task {
 
     private HashMap<Integer, SubTask> subTaskList;
-    private String title;
-    private String description;
-    private int id;
-    private Status status;
+//    private String title;
+//    private String description;
+//    private int id;
+//    private Status status;
 
     public Epic(String title, String description, int id, Status status){
-        this.title = title;
-        this.description = description;
-        this.id = id;
-        this.status = Status.NEW;
+        super(title, description, id, status);
     }
 
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public Status getStatus() {
-        return status;
-    }
-
-    @Override
-    public void setStatus(Status status) {
-        this.status = status;
+    public HashMap<Integer, SubTask> getSubTaskList() {
+        return subTaskList;
     }
 }
--- src/main/java/org/example/SubTask.java
+++ src/main/java/org/example/SubTask.java
@@ -2,49 +2,17 @@ package org.example;
 
 public class SubTask extends Task {
 
-    private String title;
-    private String description;
-    private int id;
+//    private String title;
+//    private String description;
+//    private int id;
+//    private Status status;
     private int epicId;
-    private Status status;
 
     public SubTask(String title, String description, int id, int epicId, Status status) {
-        this.title = title;
-        this.description = description;
-        this.id = id;
-        this.epicId = epicId;
-        this.status = Status.NEW;
+       super(title, description, id, status);
+       this.epicId = epicId;
     }
 
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getEpicId() {
         return epicId;
@@ -52,15 +20,5 @@ public class SubTask extends Task {
 
     public void setEpicId(int epicId) {
         this.epicId = epicId;
-    }
-
-    @Override
-    public Status getStatus() {
-        return status;
-    }
-
-    @Override
-    public void setStatus(Status status) {
-        this.status = status;
     }
 }
